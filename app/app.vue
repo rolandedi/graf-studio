98402947d861d5dee6fda20b299921339d1b4f78
Roland Edi <rolandedi@codivoire.com>
2026-06-21 21:37:57 +0000

    feat(app): replace NuxtWelcome with layout and page routing setup

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} - App` : "App";
+  },
+});
+</script>
